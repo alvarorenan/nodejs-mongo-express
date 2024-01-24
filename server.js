13e35fe8f9bf9904c67c133cfa02e14cf6c12cad
Alvaro Renan <alvaroca1544@gmail.com>
2024-01-24 16:54:58 -0300

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import app from './src/app.js';
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores",
+}
+
+
+app.listen(PORT, () => {
+    console.log("Server running at http://localhost:%d", PORT);
+});
